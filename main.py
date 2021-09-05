d11aa24b10e237c6292aa5b89c3fb1d264c34bae
rtrang <rtrang@uwaterloo.ca>
2021-09-05 16:30:31 -0400

    Created basic GUI. Experiementing with PySimpleGUI

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+import PySimpleGUI as sg
 import numpy as np
 import numpy.linalg as lin
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+
+# Content of the GUI
+GUIContent = [[sg.Text("Joint Type")], [sg.Button("Revolute")], [sg.Button("Exit")], [sg.Text("Mode")], [sg.Button("Inverse Kinematics")], [sg.Button("Forwards Kinematics")]]
+
+# Create GUI Window
+window = sg.Window(title="Build-A-Bot", layout=GUIContent, margins=(480, 270))
+
+# Create loop for GUI
+while True:
+    event, values = window.read()
+
+    # End program if user closes window or presses the OK button
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    elif event == "Revolute":
+        print("Revolute Joint")
+    elif event == "Inverse Kinematics":
+        print("Inverse Kinematics")
+    elif event == "Forwards Kinematics":
+        print("Forwards Kinematics")
+
+window.close()
