34198df5054ce6681436dd0e69427b65c89185c7
rtrang <rtrang@uwaterloo.ca>
2021-09-06 13:23:06 -0400

    Added demo code file to reference. Modified layout of GUI.

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import numpy as np
 import numpy.linalg as lin
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 # Content of the GUI
-GUIContent = [[sg.Text("Joint Type")], [sg.Button("Revolute")], [sg.Button("Exit")], [sg.Text("Mode")], [sg.Button("Inverse Kinematics")], [sg.Button("Forwards Kinematics")]]
+GUIContent = [
+    [sg.T("Joint Type")], 
+    [sg.B("Revolute")], 
+    [sg.T("Mode")], 
+    [sg.B("Inverse Kinematics"), sg.B("Forwards Kinematics")],
+    [sg.B("Exit")]
+]
 
 # Create GUI Window
 window = sg.Window(title="Build-A-Bot", layout=GUIContent, margins=(480, 270))
@@ -15,7 +22,7 @@ while True:
     event, values = window.read()
 
     # End program if user closes window or presses the OK button
-    if event == "Exit" or event == sg.WIN_CLOSED:
+    if event in (sg.WIN_CLOSED, 'Exit'):
         break
     elif event == "Revolute":
         print("Revolute Joint")
